ea182e1a10f7eec73eba9ac17a105c30d43c83a5
Mohamed-Hajri <hajr.kef@gmail.com>
2024-02-22 11:45:54 +0100

    'test'

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const app = express();
-const cors = require("cors");
-const PORT = 8000;
-
-
-
+const express = require('express')
+const app = express()
+const cors = require("cors")
+const PORT = 8000
 
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
 
-
-
-
-
+  res.send('<h1>💳hello group codeBare🛒</h1>')
+  
+})
 
 app.listen(PORT, () => {
-      console.log(`listen on http://localhost:${PORT}`);
-});
+  console.log(`Listening on http://localhost:${PORT}`)
+})
