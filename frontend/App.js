18b5e0e5192b35bb82b178e3ba56546130344a2b
abdelhakbarbouche <barbouchea0@gmail.com>
2024-02-23 15:26:59 +0100

    pull backend to do get

--- frontend/App.js
+++ frontend/App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>a7la ameni </Text>
-      hello
+      <Text> RBK </Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
