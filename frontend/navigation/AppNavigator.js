227d9dec2acb771ae4d67c3b911db06316566a0b
amenybettaher <bettaherameni1919@gmail.com>
2024-02-26 20:11:03 +0100

    testt

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignUp from '../screens/SignUp';
-
+import SignIn from '../screens/SignIn';
+// import home from '../screens/home'
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
@@ -11,6 +12,9 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="SignUp">
         <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        {/* <Stack.Screen name="home" component={home} /> */}
+
         {/* Add more screens as needed */}
       </Stack.Navigator>
     </NavigationContainer>
