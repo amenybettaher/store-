10ae6b1a55165c02baf1fbe0ea6bf0cf53bedac1
JALLELI-HIBA <jalleli.hibaa@gmail.com>
2024-03-02 21:54:38 +0100

    test

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -16,7 +16,7 @@ const Stack = createStackNavigator();
 
 function MainStack() {
   return (
-    <Stack.Navigator initialRouteName="Page" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="HomeScreen" screenOptions={{ headerShown: false }}>
 
       <Stack.Screen name='welcome' component={Page} />
       <Stack.Screen name='Onbording' component={Onbording} />
