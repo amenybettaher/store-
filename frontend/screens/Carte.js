c76f4f13a33d1cf2cc246759c24a1092d293e84d
amenybettaher <bettaherameni1919@gmail.com>
2024-03-16 02:42:39 +0100

    card css app

--- frontend/screens/Carte.js
+++ frontend/screens/Carte.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { Card } from 'react-native-elements';
+import axios from 'axios';
 
 const Carte = () => {
   const [cardData, setCardData] = useState({
@@ -18,7 +18,7 @@ const Carte = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`http://192.168.211.1:8000/carte/code/${inputCode}`);
+      const response = await axios.get(`http://192.168.1.16:8000/carte/code/${inputCode}`);
       const { points, number, code } = response.data[0]; // Adjust this according to your API response structure
       setCardData({ points, number, code });
     } catch (error) {
@@ -33,7 +33,7 @@ const Carte = () => {
   return (
     <View style={styles.container}>
       <Card containerStyle={styles.cardContainer}>
-        <Card.Title style={styles.title}>You're fidelite Card</Card.Title>
+        <Card.Title style={styles.title}>Votre Carte Fidélité</Card.Title>
         <Card.Divider />
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Enter Card Code :</Text>
@@ -45,15 +45,16 @@ const Carte = () => {
           />
         </View>
         <View style={styles.buttonContainer}>
-          <Button title="Fetch Data" onPress={handleButtonPress} />
+          <TouchableOpacity style={styles.customButton} onPress={handleButtonPress}>
+            <Text style={styles.buttonText}>get your card</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.invitationContainer}>
           <Image
-            source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpwqDMBh_dvUcPb1Vb1SOoNm6U5eG0p1Mm4UxU2SqLREXUP1fqGiYGT7hui4fTDjwvpP8&usqp=CAU' }}
+            source={require("../assets/Card (3).png")}
             style={styles.backgroundImage}
           />
           <View style={styles.textContainer}>
-            <Text style={styles.invitationText}>You're invited to an exclusive event!</Text>
             <Text style={styles.text}>Card Number: {cardData.number}</Text>
           </View>
         </View>
@@ -70,39 +71,54 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    width:500
   },
   cardContainer: {
-    backgroundColor: '#fff8e1',
+    backgroundColor: '#ffff',
     borderRadius: 10,
-    margin: 40,
+    margin: 10,
     top: -40,
     elevation: 100,
+    marginLeft:-129
   },
   title: {
     fontSize: 35,
     fontWeight: 'bold',
     textAlign: 'center',
-    color: '#673ab7',
+    color: '#7D0C43',
   },
   inputContainer: {
     marginVertical: 10,
     alignItems: 'center',
+    color:'#7D0C43'
   },
   label: {
     fontSize: 16,
     marginBottom: 5,
-    color: '#673ab7',
+    color: 'black',
   },
   input: {
     height: 40,
     width: 200,
-    borderColor: '#673ab7',
+    borderColor: '#7D0C43',
     borderWidth: 1,
     marginVertical: 10,
     paddingLeft: 10,
   },
   buttonContainer: {
     marginVertical: 10,
+  },
+  customButton: {
+    backgroundColor: '#7D0C43',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+   
   },
   invitationContainer: {
     marginTop: 20,
@@ -119,31 +135,28 @@ const styles = StyleSheet.create({
     top: 20,
     left: 20,
   },
-  invitationText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#673ab7',
-    marginBottom: 10,
-  },
   text: {
     fontSize: 16,
-    color: '#000',
-    marginBottom: 5,
+    color: '#ccc',
+    top:65,
+    marginLeft:-11
   },
   pointsContainer: {
     position: 'absolute',
     bottom: 20,
-    right: 80,
-    top: 690,
+    right: 270,
+    top: 590,
+    height: 60
   },
   pointsText: {
-    fontSize: 45,
+    fontSize: 25,
     fontWeight: 'bold',
     color: '#fff',  // Set font color to white
-    backgroundColor: '#673ab7',  // Set background color to purple
+    backgroundColor: '#7D0C43',  // Set background color to purple
     padding: 10,  // Add padding to the text
     borderRadius: 5,  // Add border radius for rounded corners
   },
 });
 
 export default Carte;
+
