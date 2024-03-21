f0a63d80889633b5d2b7fdb26c195d824880ae32
JALLELI-HIBA <jalleli.hibaa@gmail.com>
2024-03-21 21:11:10 +0100

    hiba pulling

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,7 @@ import registerNNPushToken from 'native-notify';
 
 export default function App() {
   registerNNPushToken(20289, 'JfbP2UDjztgrhHJdEPIZ9W');
+  
   return (
     <Provider store={store}>
       <AppNavigator />
